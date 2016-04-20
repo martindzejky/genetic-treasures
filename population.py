db384e9d07d681d8853aa4a8528993f07be2af67
Martin Chuckeles <martin@jakubik.info>
2016-04-20 15:33:26 +0200

    Add population score

--- population.py
+++ population.py
@@ -1,7 +1,20 @@
 import inset
+import machine
 
 
 def generate(size=100):
     """Generate a population of random insets."""
 
     return tuple(inset.generate() for i in range(size))
+
+
+def score(population, level, start):
+    """Assign fitness to every inset in the population."""
+
+    return tuple(map(lambda inset: (machine.run_path(
+                                    level,
+                                    machine.interpret_inset(inset),
+                                    start),
+                                    inset),
+                     population
+                     ))
--- test_population.py
+++ test_population.py
@@ -1,5 +1,6 @@
 import unittest
 import population
+import level
 
 
 class TestPopulation(unittest.TestCase):
@@ -7,6 +8,13 @@ class TestPopulation(unittest.TestCase):
     def test_generate(self):
         self.assertIsInstance(population.generate(), tuple)
         self.assertEqual(len(population.generate(10)), 10)
+
+    def test_score(self):
+        pop = population.generate()
+
+        for pair in population.score(pop, level.generate(), (0, 0)):
+            self.assertGreaterEqual(pair[0], 1)
+            self.assertIsInstance(pair[1], tuple)
 
 
 if __name__ == "__main__":
