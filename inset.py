67520953d18fcc92aed67f4a9f4322fbb1af3de7
Martin Chuckeles <martin@jakubik.info>
2016-04-20 18:09:54 +0200

    Mutate 1 bit only

--- inset.py
+++ inset.py
@@ -14,10 +14,20 @@ def crossover(parent1, parent2):
     return parent1[:point] + parent2[point:], parent2[:point] + parent1[point:]
 
 
+def change_bit(byte):
+    """Change 1 bit in a byte randomly."""
+
+    bit = 1 << random.randrange(8)
+    if random.randrange(10) < 5:
+        return byte | bit
+    else:
+        return byte & ~bit
+
+
 def mutate(inset, mutation_chance=5):
     """Mutate an inset."""
 
     return tuple(i if random.randrange(100) > mutation_chance
-                 else random.randrange(255)
+                 else change_bit(i)
                  for i
                  in inset)
