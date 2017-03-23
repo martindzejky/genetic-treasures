7fdb5f8a28c069ba2cae8a8789c45de7c096986d
Martin Jakubik <martin@jakubik.info>
2017-03-23 12:04:19 +0100

    Add the instruction set and tests for it

---
+++ instruction_set.py
@@ -0,0 +1,47 @@
+import random
+
+
+def generate(size=64):
+    """ Generate a new instruction set. """
+
+    return [random.randrange(256) for _ in range(size)]
+
+
+def crossover(parent1, parent2, take_random=False):
+    """ Cross-over 2 instruction sets. If take_random is False, selects just 1 point and
+        takes the first part from set1 and the second from set2. If take_random is True,
+        each instruction is taken from either set randomly. """
+
+    if take_random:
+        indices = [random.choice([1, 2]) for _ in parent1]
+        child1 = []
+        child2 = []
+
+        for i, index in enumerate(indices):
+            child1.append(parent1[i] if index == 1 else parent2[i])
+            child2.append(parent1[i] if index == 2 else parent2[i])
+
+        return child1, child2
+
+    else:
+        point = random.randrange(len(parent1))
+        return parent2[point:] + parent1[:point], parent1[point:] + parent2[:point]
+
+
+def mutate_bits(inset, mutation_chance=5):
+    """ Mutate the instruction set by changing 1 bit per instruction. """
+
+    def change_bit(byte):
+        bit = 1 << random.randrange(8)
+        if random.choice([True, False]):
+            return byte | bit
+        else:
+            return byte & ~bit
+
+    return [change_bit(i) if random.randrange(100) < mutation_chance else i for i in inset]
+
+
+def mutate_bytes(inset, mutation_chance=2):
+    """ Mutate the instruction set by changing whole bytes. """
+
+    return [random.randrange(256) if random.randrange(100) < mutation_chance else i for i in inset]
---
+++ instruction_set.test.py
@@ -0,0 +1,57 @@
+import unittest
+import instruction_set
+
+
+class TestInstructionSet(unittest.TestCase):
+    def test_generate(self):
+        self.assertIsInstance(instruction_set.generate(), list)
+
+        self.assertEqual(len(instruction_set.generate()), 64)
+        self.assertEqual(len(instruction_set.generate(32)), 32)
+
+        inset = instruction_set.generate()
+        for instruction in inset:
+            self.assertGreaterEqual(instruction, 0)
+            self.assertLess(instruction, 256)
+
+    def test_crossover(self):
+        parent1 = instruction_set.generate()
+        parent2 = instruction_set.generate()
+
+        children = instruction_set.crossover(parent1, parent2)
+        random_children = instruction_set.crossover(parent1, parent2, take_random=True)
+
+        self.assertIsInstance(children, tuple)
+        self.assertIsInstance(children[0], list)
+        self.assertIsInstance(children[1], list)
+
+        self.assertEqual(len(children[0]), len(parent1))
+        self.assertEqual(len(children[1]), len(parent1))
+
+        for i in range(len(parent1)):
+            self.assertTrue(
+                (children[0][i] in parent1 and children[1][i] in parent2) or
+                (children[0][i] in parent2 and children[1][i] in parent1)
+            )
+            self.assertTrue(
+                (random_children[0][i] in parent1 and random_children[1][i] in parent2) or
+                (random_children[0][i] in parent2 and random_children[1][i] in parent1)
+            )
+
+    def test_mutate_bits(self):
+        inset = instruction_set.generate()
+
+        self.assertEqual(len(inset), len(instruction_set.mutate_bits(inset)))
+        self.assertEqual(inset, instruction_set.mutate_bits(inset, mutation_chance=0))
+        self.assertNotEqual(inset, instruction_set.mutate_bits(inset, mutation_chance=100))
+
+    def test_mutate_bytes(self):
+        inset = instruction_set.generate()
+
+        self.assertEqual(len(inset), len(instruction_set.mutate_bytes(inset)))
+        self.assertEqual(inset, instruction_set.mutate_bytes(inset, mutation_chance=0))
+        self.assertNotEqual(inset, instruction_set.mutate_bytes(inset, mutation_chance=100))
+
+
+if __name__ == '__main__':
+    unittest.main()
